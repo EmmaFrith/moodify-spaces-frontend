8fe7d0c582acd803afe1a852ffca74f5575961a3
Emma Frith <emma.frith@education.gov.uk>
2024-07-08 15:38:22 +0100

    link to backend

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return <div className="hero is-fullheight-with-navbar">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="subtitle mb-5">
+            Hello
+          </h1>
+        </div>
+      </div>
+    </div>
+  }
+  
+  export default Home
