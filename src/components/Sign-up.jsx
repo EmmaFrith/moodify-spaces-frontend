386896a1603f8d624948ea6c282daf0f515fdfea
Emma Frith <emma.frith@education.gov.uk>
2024-07-15 12:18:57 +0100

    get frontend ready for deployment

--- src/components/Sign-up.jsx
+++ src/components/Sign-up.jsx
@@ -3,6 +3,7 @@ import axios from "axios"
 import { useNavigate } from "react-router-dom"
 import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify'
+import { baseUrl } from '../config'
 
 export default function Signup() {
 
@@ -26,7 +27,7 @@ export default function Signup() {
     async function handleSubmit(e) {
         e.preventDefault();
         try {
-            await axios.post(`http://localhost:8000/api/auth/register/`, formData);
+            await axios.post(`${baseUrl}/api/auth/register/`, formData);
             toast.success('Signup successful');
             navigate('/');
             
